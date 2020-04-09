0a84fea553dc6e52dcc1218c57ef5d791f0ad73b
geranium236 <geranium236@gmail.com>
2020-04-10 00:08:02 +0900

    add a new package and java file

---
+++ TeamProject_REAL/src/forGit/Master_Confilct_test.java
@@ -0,0 +1,10 @@
+package forGit;
+
+public class Master_Confilct_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Master commit");
+	}
+
+}
