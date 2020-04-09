332753bb22f36cf50db38f8b869a17e1f0738033
geranium236 <geranium236@gmail.com>
2020-04-10 00:29:15 +0900

    Testing stash

--- TeamProject_REAL/src/forGit/Master_Confilct_test.java
+++ TeamProject_REAL/src/forGit/Master_Confilct_test.java
@@ -5,6 +5,7 @@ public class Master_Confilct_test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Master commit");
+		System.out.println("Testing stash");
 	}
 
 }
